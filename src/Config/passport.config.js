298a3831d97ae39e88b4869515d2058559a832d6
Matías De Marco <demarcomatias25@gmail.com>
2024-05-06 20:36:44 -0300

    -

--- src/Config/passport.config.js
+++ src/Config/passport.config.js
@@ -69,7 +69,7 @@ exports.initializePassportLocal = () => {
         { usernameField: 'email', passwordField: 'password' },
         async (email, password, done) => {
             try {
-                const user = await User.findOne({ email }).maxTimeMS(1000).exec()
+                const user = await User.find({ email }).maxTimeMS(1000).exec()
                 if (!user) {
                     return done(null, false, { message: 'Email o contraseña incorrectos' });
                 }
