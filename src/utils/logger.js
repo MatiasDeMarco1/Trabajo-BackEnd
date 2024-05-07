808ec4c5314bfb562ba1b75344648a570ce35d8f
Matías De Marco <demarcomatias25@gmail.com>
2024-05-06 22:56:06 -0300

    -

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,23 +1,48 @@
-const { createLogger, format, transports } = require('winston');
-const { combine, timestamp, printf } = format;
+const winston = require( 'winston')
 
-const logFormat = printf(({ level, message, timestamp }) => {
-    return `${timestamp} ${level}: ${message}`;
-});
 
-const developmentTransport = new transports.Console();
-const productionTransport = new transports.File({ filename: 'errors.log', level: 'debug' });
+const customLevelOptions = {
+        levels: {
+            fatal: 0,
+            error: 1,
+            warning: 2,
+            info: 3,
+            debug: 4
+        },
+        colors: {
+            fatal: 'red',
+            error: 'yellow',
+            warning: 'yellow',
+            info: 'blue',
+            debug: 'white'
+        }
+}
 
-const logger = createLogger({
-    level: 'debug',
-    format: combine(
-        timestamp(),
-        logFormat
-    ),
+const logger = winston.createLogger({
+    levels: customLevelOptions.levels,
     transports: [
-        developmentTransport,
-        productionTransport
-    ],
-});
+        new winston.transports.Console({
+            level:'info',
+            format: winston.format.combine(
+                winston.format.colorize({colors: customLevelOptions.colors}),
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.File({
+            filename: './errors.log',
+            level: 'warning',
+            format: winston.format.simple()
+        })
+    ]
+})
 
-module.exports = logger;
+const addLogger =(req, res, next ) => {
+    req.logger = logger
+    logger.info(`${req.method} Ruta: localhost:8080${req.url} - ${new Date().getDate}`)
+    next();
+}
+
+module.exports = {
+    addLogger,
+    logger
+}
