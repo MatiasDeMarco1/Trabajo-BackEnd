a487eee2714c651ca4e1d080e8ceca96e650ff1b
Matías De Marco <demarcomatias25@gmail.com>
2024-05-06 21:38:45 -0300

    -

--- src/app.js
+++ src/app.js
@@ -144,7 +144,7 @@ io.on("connection", (socket) => {
         }
     });
 });
-
+console.log(MONGO_URL)
 serverHTTP.listen(PORT, () => {
     logger.info(`Servidor escuchando en http://localhost:${PORT}`);
 });
